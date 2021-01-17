d8e05b9559c866072f798e0f60a172d9cfbdcf3c
Alessandro Privitera <alexprivitera28@gmail.com>
2021-01-17 22:15:08 +0100

    Update
    Co-Authored-By: Samuele Girgenti <samuele.girgenti458@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Button } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import TextField from '@material-ui/core/TextField';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <React.Fragment>
+
+<AppBar position="static">
+  <Toolbar>
+    <IconButton edge="start"  color="inherit" aria-label="menu">
+
+    </IconButton>
+    <Typography variant="h6" >
+Clip Snaut    </Typography>
+
+  </Toolbar>
+</AppBar>
+
+
+      <Container maxWidth="md">
+
+
+
+
+        <Grid item xs={12}>
+
+
+
+        <form noValidate autoComplete="off">
+  <TextField id="standard-basic" label="Standard" />
+</form>
+
+        </Grid>
+
+      </Container>
+
+    </React.Fragment>
   );
 }
 
